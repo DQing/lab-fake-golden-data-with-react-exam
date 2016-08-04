26551e5e12a0df73f89c6416de77840aa5429c5a
douqing <15829258737@163.com>
2016-08-04 20:06:03 +0800

    创建App组件以及两个子组件

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-// TODO
+const App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <button>Preview</button>
+                <Preview />
+                <Editor />
+            </div>
+        )
+    }
+});
+const Preview = React.createClass({
+    render: function () {
+        return (
+            <div>Preview</div>
+        )
+    }
+});
+const Editor = React.createClass({
+    render: function () {
+        return (
+            <div>Editor</div>
+        )
+    }
+});
+ReactDOM.render(<App />, document.getElementById('content'));
