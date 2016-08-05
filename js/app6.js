bbc190d39287b75c87df85444b24fecc4eb69bf1
douqing <15829258737@163.com>
2016-08-05 09:44:42 +0800

    创建组件,以及整体结构

---
+++ js/app6.js
@@ -0,0 +1,26 @@
+const App = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <button>isEditor</button>
+                <Preview />
+                <Editor />
+            </div>
+        )
+    }
+});
+const Preview = React.createClass({
+    render: function () {
+        return (
+            <div>Preview</div>
+        )
+    }
+});
+const Editor = React.createClass({
+    render: function () {
+        return (
+            <div>Editor</div>
+        )
+    }
+});
+ReactDOM.render(<App />, document.getElementById('content'));
