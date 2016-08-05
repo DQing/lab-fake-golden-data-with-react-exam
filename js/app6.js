5c73e5819c22886dd52f91c2c25880e9c1cbb751
douqing <15829258737@163.com>
2016-08-05 09:48:54 +0800

    完成页面的跳转

--- js/app6.js
+++ js/app6.js
@@ -1,21 +1,25 @@
 const App = React.createClass({
     getInitialState(){
-        return{
-            isEditor:true
+        return {
+            isEditor: true
         }
     },
-    toggle:function () {
-      this.setState({
-          isEditor:!this.state.isEditor
-      })
+    toggle: function () {
+        this.setState({
+            isEditor: !this.state.isEditor
+        })
     },
     render: function () {
         const isEditor = this.state.isEditor;
         return (
             <div>
-                <button onClick = {this.toggle}>{isEditor?"Preview":"Editor"}</button>
-                <Preview />
-                <Editor />
+                <button onClick={this.toggle}>{isEditor ? "Preview" : "Editor"}</button>
+                <div className={isEditor?"hidden":""}>
+                    <Preview />
+                </div>
+                <div className={isEditor?"":"hidden"}>
+                    <Editor />
+                </div>
             </div>
         )
     }
